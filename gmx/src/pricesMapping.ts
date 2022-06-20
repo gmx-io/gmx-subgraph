f9c64865540029de90d7680044954b5c3059356d
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-06-20 21:37:18 +0300

    add blockNumber to prices

--- gmx/src/pricesMapping.ts
+++ gmx/src/pricesMapping.ts
@@ -40,13 +40,14 @@ import {
   Swap as UniswapSwap
 } from '../generated/UniswapPool/UniswapPoolV3'
 
-function _storeChainlinkPrice(token: string, value: BigInt, timestamp: BigInt): void {
+function _storeChainlinkPrice(token: string, value: BigInt, timestamp: BigInt, blockNumber: BigInt): void {
   let id = token + ":" + timestamp.toString()
   let entity = new ChainlinkPrice(id)
   entity.value = value
   entity.period = "any"
   entity.token = token
   entity.timestamp = timestamp.toI32()
+  entity.blockNumber = blockNumber.toI32()
   entity.save()
 
   let totalId = token
@@ -55,48 +56,57 @@ function _storeChainlinkPrice(token: string, value: BigInt, timestamp: BigInt): 
   totalEntity.period = "last"
   totalEntity.token = token
   totalEntity.timestamp = timestamp.toI32()
+  totalEntity.blockNumber = blockNumber.toI32()
   totalEntity.save()
 }
 
 export function handleAnswerUpdatedBTC(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(BTC, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(BTC, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedETH(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(WETH, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(WETH, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedUNI(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(UNI, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(UNI, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedLINK(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(LINK, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(LINK, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedSPELL(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(SPELL, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(SPELL, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedMIM(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(MIM, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(MIM, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedDAI(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(DAI, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(DAI, event.params.current, event.block.timestamp, event.block.number)
 }
 
 export function handleAnswerUpdatedSUSHI(event: AnswerUpdatedEvent): void {
-  _storeChainlinkPrice(SUSHI, event.params.current, event.block.timestamp)
+  _storeChainlinkPrice(SUSHI, event.params.current, event.block.timestamp, event.block.number)
 }
 
-function _storeUniswapPrice(id: string, token: string, price: BigInt, period: string, timestamp: BigInt): void {
+function _storeUniswapPrice(
+  id: string,
+  token: string,
+  price: BigInt,
+  period: string,
+  timestamp: BigInt,
+  blockNumber: BigInt
+): void {
   let entity = UniswapPrice.load(id)
   if (entity == null) {
     entity = new UniswapPrice(id)
   }
 
   entity.timestamp = timestamp.toI32()
+  entity.blockNumber = blockNumber.toI32()
   entity.value = price
   entity.token = token
   entity.period = period
@@ -108,36 +118,44 @@ export function handleUniswapGmxEthSwap(event: UniswapSwap): void {
   let gmxPrice = getTokenAmountUsd(WETH, ethPerGmx)
 
   let totalId = GMX
-  _storeUniswapPrice(totalId, GMX, gmxPrice, "last", event.block.timestamp)
+  _storeUniswapPrice(totalId, GMX, gmxPrice, "last", event.block.timestamp, event.block.number)
 
   let id = GMX + ":" + event.block.timestamp.toString()
-  _storeUniswapPrice(id, GMX, gmxPrice, "any", event.block.timestamp)
+  _storeUniswapPrice(id, GMX, gmxPrice, "any", event.block.timestamp, event.block.number)
 }
 
-function _handleFastPriceUpdate(token: Address, price: BigInt, timestamp: BigInt): void {
+function _handleFastPriceUpdate(token: Address, price: BigInt, timestamp: BigInt, blockNumber: BigInt): void {
   let dailyTimestampGroup = timestampToPeriod(timestamp, "daily")
-  _storeFastPrice(dailyTimestampGroup.toString() + ":daily:" + token.toHexString(), token, price, dailyTimestampGroup, "daily")
+  _storeFastPrice(dailyTimestampGroup.toString() + ":daily:" + token.toHexString(), token, price, dailyTimestampGroup, blockNumber, "daily")
 
   let hourlyTimestampGroup = timestampToPeriod(timestamp, "hourly")
-  _storeFastPrice(hourlyTimestampGroup.toString() + ":hourly:" + token.toHexString(), token, price, hourlyTimestampGroup, "hourly")
+  _storeFastPrice(hourlyTimestampGroup.toString() + ":hourly:" + token.toHexString(), token, price, hourlyTimestampGroup, blockNumber, "hourly")
 
-  _storeFastPrice(timestamp.toString() + ":any:" + token.toHexString(), token, price, timestamp, "any")
-  _storeFastPrice(token.toHexString(), token, price, timestamp, "last")
+  _storeFastPrice(timestamp.toString() + ":any:" + token.toHexString(), token, price, timestamp, blockNumber, "any")
+  _storeFastPrice(token.toHexString(), token, price, timestamp, blockNumber, "last")
 }
 
-function _storeFastPrice(id: string, token: Address, price: BigInt, timestampGroup: BigInt, period: string): void {
+function _storeFastPrice(
+  id: string,
+  token: Address,
+  price: BigInt,
+  timestampGroup: BigInt,
+  blockNumber: BigInt,
+  period: string,
+): void {
   let entity = new FastPrice(id)
   entity.period = period
   entity.value = price
   entity.token = token.toHexString()
   entity.timestamp = timestampGroup.toI32()
+  entity.blockNumber = blockNumber.toI32()
   entity.save()
 }
 
 export function handlePriceUpdate(event: PriceUpdate): void {
-  _handleFastPriceUpdate(event.params.token, event.params.price, event.block.timestamp)
+  _handleFastPriceUpdate(event.params.token, event.params.price, event.block.timestamp, event.block.number)
 }
 
 export function handleSetPrice(event: SetPrice): void {
-  _handleFastPriceUpdate(event.params.token, event.params.price, event.block.timestamp)
+  _handleFastPriceUpdate(event.params.token, event.params.price, event.block.timestamp, event.block.number)
 }
