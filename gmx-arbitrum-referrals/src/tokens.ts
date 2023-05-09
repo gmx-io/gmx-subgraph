6e68f07d0fb8d8393f1302425f7b81dc3d6eafb2
divhead <i.divhead@gmail.com>
2023-05-09 05:46:47 +0400

    (synthetics): upd token addresses

--- gmx-arbitrum-referrals/src/tokens.ts
+++ gmx-arbitrum-referrals/src/tokens.ts
@@ -1,35 +1,35 @@
-let ETH_PRICE_FEED = "0xf829c9aa5569ae7fc047b2344631778069f3fe39"
-let AVAX_PRICE_FEED = "0x6c2441920404835155f33d88faf0545b895871b1"
-let USDC_PRICE_FEED = "0xa47f3fac2273d138295aef7b4326630f301a3ab2"
-let USDT_PRICE_FEED = "0xa47f3fac2273d138295aef7b4326630f301a3ab2" // same as USDC
+let ETH_PRICE_FEED = "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612";
+let AVAX_PRICE_FEED = "0x8bf61728eeDCE2F32c456454d87B5d6eD6150208";
+let USDC_PRICE_FEED = "0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7";
+let USDT_PRICE_FEED = "0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7"; // same as USDC
 
-let WETH = "0x82f0b3695ed2324e55bbd9a9554cb4192ec3a514"
-let AVAX = "0x1d308089a2d1ced3f1ce36b1fcaf815b07217be3"
-let USDC = "0x3ebdeaa0db3ffde96e7a0dbbafec961fc50f725f"
-let USDT = "0x6931ec3e364245e6d093afa1f2e96cce3f17538b"
+let WETH = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
+let AVAX = "0x565609fAF65B92F7be02468acF86f8979423e514";
+let USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
+let USDT = "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9";
 
 export function getTokenByPriceFeed(priceFeed: string): string[] {
-    if (priceFeed == ETH_PRICE_FEED) {
-        return [WETH]
-    } else if (priceFeed == AVAX_PRICE_FEED) {
-        return [AVAX]
-    } else if (priceFeed == USDC_PRICE_FEED || priceFeed == USDT_PRICE_FEED) {
-        return [USDC, USDT]
-    }
-    
-    return []
+  if (priceFeed == ETH_PRICE_FEED) {
+    return [WETH];
+  } else if (priceFeed == AVAX_PRICE_FEED) {
+    return [AVAX];
+  } else if (priceFeed == USDC_PRICE_FEED || priceFeed == USDT_PRICE_FEED) {
+    return [USDC, USDT];
+  }
+
+  return [];
 }
 
 export function getTokenDecimals(tokenAddress: string): number {
-    if (tokenAddress == WETH) {
-        return 18
-    } else if (tokenAddress == AVAX) {
-        return 18
-    } else if (tokenAddress == USDC) {
-        return 6
-    } else if (tokenAddress == USDT) {
-        return 6
-    }
-    
-    return 0
+  if (tokenAddress == WETH) {
+    return 18;
+  } else if (tokenAddress == AVAX) {
+    return 18;
+  } else if (tokenAddress == USDC) {
+    return 6;
+  } else if (tokenAddress == USDT) {
+    return 6;
+  }
+
+  return 0;
 }
