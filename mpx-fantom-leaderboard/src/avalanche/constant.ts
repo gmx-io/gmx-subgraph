bb447c2dc0b099ea5dbc56c0e1a83b77ca95b27b
daedboi <87483308+daedboi@users.noreply.github.com>
2023-08-08 07:16:51 +0300

    Leaderboard fixes

--- mpx-fantom-leaderboard/src/avalanche/constant.ts
+++
@@ -1,8 +0,0 @@
-
-export const WAVAX = "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"
-export const WETHE = "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab"
-export const WBTCE = "0x50b7545627a5162f82a992c33b87adc75187b218"
-export const BTCB = "0x152b9d0fdc40c096757f570a51e494bd4b943e50"
-export const GLP = "0x01234181085565ed162a948b6a5e88758cd7c7b8"
-export const GMX = "0x62edc0692bd897d2295872a9ffcac5425011c661"
-export const EsGMX = "0xff1489227bbaac61a9209a08929e4c2a526ddd17"
