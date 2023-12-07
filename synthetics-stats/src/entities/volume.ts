2cb154d55c5a33ac26bf74044b7d9c5a46e7c90e
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-12-07 14:29:52 +0400

    fix volume info id collision

--- synthetics-stats/src/entities/volume.ts
+++ synthetics-stats/src/entities/volume.ts
@@ -1,18 +1,9 @@
 import { BigInt, log } from "@graphprotocol/graph-ts";
-import {
-  MarketInfo,
-  PositionVolumeInfo,
-  SwapVolumeInfo,
-  VolumeInfo,
-} from "../../generated/schema";
+import { MarketInfo, PositionVolumeInfo, SwapVolumeInfo, VolumeInfo } from "../../generated/schema";
 import { timestampToPeriodStart } from "../utils/time";
 import { getMarketInfo } from "./markets";
 
-export function saveVolumeInfo(
-  type: string,
-  timestamp: i32,
-  volume: BigInt
-): void {
+export function saveVolumeInfo(type: string, timestamp: i32, volume: BigInt): void {
   let hourlyVolumeInfo = getOrCreateVolumeInfo(timestamp, "1h");
   let dailyVolumeInfo = getOrCreateVolumeInfo(timestamp, "1d");
   let totalVolumeInfo = getOrCreateVolumeInfo(timestamp, "total");
@@ -22,47 +13,27 @@ export function saveVolumeInfo(
   totalVolumeInfo.volumeUsd = totalVolumeInfo.volumeUsd.plus(volume);
 
   if (type == "swap") {
-    hourlyVolumeInfo.swapVolumeUsd = hourlyVolumeInfo.swapVolumeUsd.plus(
-      volume
-    );
+    hourlyVolumeInfo.swapVolumeUsd = hourlyVolumeInfo.swapVolumeUsd.plus(volume);
     dailyVolumeInfo.swapVolumeUsd = dailyVolumeInfo.swapVolumeUsd.plus(volume);
     totalVolumeInfo.swapVolumeUsd = totalVolumeInfo.swapVolumeUsd.plus(volume);
   }
 
   if (type == "deposit") {
-    hourlyVolumeInfo.depositVolumeUsd = hourlyVolumeInfo.depositVolumeUsd.plus(
-      volume
-    );
-    dailyVolumeInfo.depositVolumeUsd = dailyVolumeInfo.depositVolumeUsd.plus(
-      volume
-    );
-    totalVolumeInfo.depositVolumeUsd = totalVolumeInfo.depositVolumeUsd.plus(
-      volume
-    );
+    hourlyVolumeInfo.depositVolumeUsd = hourlyVolumeInfo.depositVolumeUsd.plus(volume);
+    dailyVolumeInfo.depositVolumeUsd = dailyVolumeInfo.depositVolumeUsd.plus(volume);
+    totalVolumeInfo.depositVolumeUsd = totalVolumeInfo.depositVolumeUsd.plus(volume);
   }
 
   if (type == "withdrawal") {
-    hourlyVolumeInfo.withdrawalVolumeUsd = hourlyVolumeInfo.withdrawalVolumeUsd.plus(
-      volume
-    );
-    dailyVolumeInfo.withdrawalVolumeUsd = dailyVolumeInfo.withdrawalVolumeUsd.plus(
-      volume
-    );
-    totalVolumeInfo.withdrawalVolumeUsd = totalVolumeInfo.withdrawalVolumeUsd.plus(
-      volume
-    );
+    hourlyVolumeInfo.withdrawalVolumeUsd = hourlyVolumeInfo.withdrawalVolumeUsd.plus(volume);
+    dailyVolumeInfo.withdrawalVolumeUsd = dailyVolumeInfo.withdrawalVolumeUsd.plus(volume);
+    totalVolumeInfo.withdrawalVolumeUsd = totalVolumeInfo.withdrawalVolumeUsd.plus(volume);
   }
 
   if (type == "margin") {
-    hourlyVolumeInfo.marginVolumeUsd = hourlyVolumeInfo.marginVolumeUsd.plus(
-      volume
-    );
-    dailyVolumeInfo.marginVolumeUsd = dailyVolumeInfo.marginVolumeUsd.plus(
-      volume
-    );
-    totalVolumeInfo.marginVolumeUsd = totalVolumeInfo.marginVolumeUsd.plus(
-      volume
-    );
+    hourlyVolumeInfo.marginVolumeUsd = hourlyVolumeInfo.marginVolumeUsd.plus(volume);
+    dailyVolumeInfo.marginVolumeUsd = dailyVolumeInfo.marginVolumeUsd.plus(volume);
+    totalVolumeInfo.marginVolumeUsd = totalVolumeInfo.marginVolumeUsd.plus(volume);
   }
 
   hourlyVolumeInfo.save();
@@ -87,30 +58,10 @@ function getOrCreateVolumeInfo(timestamp: i32, period: string): VolumeInfo {
   return volumeInfo as VolumeInfo;
 }
 
-export function saveSwapVolumeInfo(
-  timestamp: i32,
-  tokenIn: string,
-  tokenOut: string,
-  volumeUsd: BigInt
-): void {
-  let hourlyVolumeInfo = getOrCreateSwapVolumeInfo(
-    timestamp,
-    tokenIn,
-    tokenOut,
-    "1h"
-  );
-  let dailyVolumeInfo = getOrCreateSwapVolumeInfo(
-    timestamp,
-    tokenIn,
-    tokenOut,
-    "1d"
-  );
-  let totalVolumeInfo = getOrCreateSwapVolumeInfo(
-    timestamp,
-    tokenIn,
-    tokenOut,
-    "total"
-  );
+export function saveSwapVolumeInfo(timestamp: i32, tokenIn: string, tokenOut: string, volumeUsd: BigInt): void {
+  let hourlyVolumeInfo = getOrCreateSwapVolumeInfo(timestamp, tokenIn, tokenOut, "1h");
+  let dailyVolumeInfo = getOrCreateSwapVolumeInfo(timestamp, tokenIn, tokenOut, "1d");
+  let totalVolumeInfo = getOrCreateSwapVolumeInfo(timestamp, tokenIn, tokenOut, "total");
 
   hourlyVolumeInfo.volumeUsd = hourlyVolumeInfo.volumeUsd.plus(volumeUsd);
   dailyVolumeInfo.volumeUsd = dailyVolumeInfo.volumeUsd.plus(volumeUsd);
@@ -121,17 +72,13 @@ export function saveSwapVolumeInfo(
   totalVolumeInfo.save();
 }
 
-function getOrCreateSwapVolumeInfo(
-  timestamp: i32,
-  tokenIn: string,
-  tokenOut: string,
-  period: string
-): SwapVolumeInfo {
+function getOrCreateSwapVolumeInfo(timestamp: i32, tokenIn: string, tokenOut: string, period: string): SwapVolumeInfo {
   let timestampGroup = timestampToPeriodStart(timestamp, period);
-  let id =
-    period === "total"
-      ? "total"
-      : getVolumeInfoId(tokenIn, tokenOut) + ":" + timestampGroup.toString();
+
+  let id = getVolumeInfoId(tokenIn, tokenOut) + ":" + period;
+  if (period != "total") {
+    id = id + ":" + timestampGroup.toString();
+  }
   let volumeInfo = SwapVolumeInfo.load(id);
 
   if (volumeInfo === null) {
@@ -152,24 +99,9 @@ export function savePositionVolumeInfo(
   sizeInUsd: BigInt
 ): void {
   let marketInfo = getMarketInfo(marketToken);
-  let hourlyVolumeInfo = getOrCreatePositionVolumeInfo(
-    timestamp,
-    collateralToken,
-    marketInfo.indexToken,
-    "1h"
-  );
-  let dailyVolumeInfo = getOrCreatePositionVolumeInfo(
-    timestamp,
-    collateralToken,
-    marketInfo.indexToken,
-    "1d"
-  );
-  let totalVolumeInfo = getOrCreatePositionVolumeInfo(
-    timestamp,
-    collateralToken,
-    marketInfo.indexToken,
-    "total"
-  );
+  let hourlyVolumeInfo = getOrCreatePositionVolumeInfo(timestamp, collateralToken, marketInfo.indexToken, "1h");
+  let dailyVolumeInfo = getOrCreatePositionVolumeInfo(timestamp, collateralToken, marketInfo.indexToken, "1d");
+  let totalVolumeInfo = getOrCreatePositionVolumeInfo(timestamp, collateralToken, marketInfo.indexToken, "total");
 
   hourlyVolumeInfo.volumeUsd = hourlyVolumeInfo.volumeUsd.plus(sizeInUsd);
   dailyVolumeInfo.volumeUsd = dailyVolumeInfo.volumeUsd.plus(sizeInUsd);
@@ -187,12 +119,10 @@ function getOrCreatePositionVolumeInfo(
   period: string
 ): PositionVolumeInfo {
   let timestampGroup = timestampToPeriodStart(timestamp, period);
-  let id =
-    period === "total"
-      ? "total"
-      : getVolumeInfoId(collateralToken, indexToken) +
-        ":" +
-        timestampGroup.toString();
+  let id = getVolumeInfoId(collateralToken, indexToken) + ":" + period;
+  if (period != "total") {
+    id = id + ":" + timestampGroup.toString();
+  }
   let volumeInfo = PositionVolumeInfo.load(id);
 
   if (volumeInfo === null) {
