e3adbd2283a50641dbcca18f3b40d516368359cc
Crolev <crolev.eth@gmail.com>
2024-05-07 20:38:26 +0200

    switch epoch start timestamp

--- symmio-stats/config/config.ts
+++ symmio-stats/config/config.ts
@@ -1,3 +1,3 @@
-export const ID = "based"
-export const EPOCH_START_TIMESTAMP = 1697932800
+export const ID = "bmx"
+export const EPOCH_START_TIMESTAMP = 1707955200
 export const MULTI_ACCOUNT_ADDRESS = "0x1c03B6480a4efC2d4123ba90d7857f0e1878B780"
