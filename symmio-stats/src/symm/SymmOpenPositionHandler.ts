6a8acd7f67f5a15c274bb0634f50568c31df0c3b
Crolev <crolev.eth@gmail.com>
2024-06-10 22:12:53 +0200

    fix overcount for liquidations in symmio rewarder stats

--- symmio-stats/src/symm/SymmOpenPositionHandler.ts
+++ symmio-stats/src/symm/SymmOpenPositionHandler.ts
@@ -26,13 +26,7 @@ export class OpenPositionHandler extends Handler {
     if (this.user == zero_address) return
     const volumeInDollars = this.getVolume()
     updateVolume(this.user, this.day, this.week, volumeInDollars, this.timestamp) // user volume tracker
-    updateVolume(
-      Address.fromBytes(zero_address),
-      this.day,
-      this.week,
-      volumeInDollars,
-      this.timestamp,
-    ) // total volume tracker
+    updateVolume(Address.fromBytes(zero_address), this.day, this.week, volumeInDollars, this.timestamp) // total volume tracker
 
     const quote = Quote.load(this.getQuoteObjectId(this.event.params.quoteId))
     if (quote == null) return // FIXME: should not happen !
