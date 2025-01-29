31d54e8495e2d48dd39d5f56f11cdb0f0c073951
Crolev <crolev.eth@gmail.com>
2025-01-29 22:52:42 +0100

    fix mode leaderboard

--- bmx-mode-leaderboard/src/arbitrum/reward.ts
+++ bmx-mode-leaderboard/src/arbitrum/reward.ts
@@ -1,13 +1,38 @@
 import * as erc20 from "../../generated/transferGmx/ERC20"
 import { getTokenUsdAmount, _storeERC20Transfer, TokenDecimals, getIdFromEvent } from "../helpers"
-import { BLT, BMX } from "./constant"
+import { BMX, MLT } from "./constant"
+import {BigInt} from "@graphprotocol/graph-ts";
+
+export const ZERO_BI = BigInt.fromI32(0)
 
 export function handleGmxTransfer(event: erc20.Transfer): void {
-  const amountUsd = getTokenUsdAmount(event.params.value, BMX, TokenDecimals.BMX)
+  // const amountUsd = getTokenUsdAmount(event.params.value, BMX, TokenDecimals.BMX)
+  const amountUsd = ZERO_BI
   _storeERC20Transfer(BMX, event, amountUsd)
 }
 
 export function handleGlpTransfer(event: erc20.Transfer): void {
-  const amountUsd = getTokenUsdAmount(event.params.value, BLT, TokenDecimals.BLT)
-  _storeERC20Transfer(BLT, event, amountUsd)
+  // const amountUsd = getTokenUsdAmount(event.params.value, MLT, TokenDecimals.MLT)
+  const amountUsd = ZERO_BI
+  _storeERC20Transfer(MLT, event, amountUsd)
 }
+// WETH, WBTC, weETH, MODE,
+// export function handleWETHTransfer(event: erc20.Transfer): void {
+//   const amountUsd = getTokenUsdAmount(event.params.value, WETH, TokenDecimals.WETH)
+//   _storeERC20Transfer(WETH, event, amountUsd)
+// }
+//
+// export function handleWBTCTransfer(event: erc20.Transfer): void {
+//   const amountUsd = getTokenUsdAmount(event.params.value, WBTC, TokenDecimals.WBTC)
+//   _storeERC20Transfer(WBTC, event, amountUsd)
+// }
+//
+// export function handleweETHTransfer(event: erc20.Transfer): void {
+//   const amountUsd = getTokenUsdAmount(event.params.value, weETH, TokenDecimals.weETH)
+//   _storeERC20Transfer(weETH, event, amountUsd)
+// }
+//
+// export function handleweMODETransfer(event: erc20.Transfer): void {
+//   const amountUsd = getTokenUsdAmount(event.params.value, MODE, TokenDecimals.MODE)
+//   _storeERC20Transfer(MODE, event, amountUsd)
+// }
--- bmx-mode-leaderboard/src/arbitrum/tokenPrice.ts
+++ bmx-mode-leaderboard/src/arbitrum/tokenPrice.ts
@@ -1,8 +1,8 @@
 import { PriceUpdate } from '../../generated/FastPriceFeed/FastPriceEvents'
 import { AddLiquidity, RemoveLiquidity } from "../../generated/GlpManager/GlpManager"
-import { Sync } from '../../generated/GmxPrice/UniswapPoolV3'
-import { getTokenUsdAmount, TokenDecimals, _storeDefaultPricefeed, BI_18_PRECISION, _storeGlpAddLiqPricefeed, _storeGlpRemoveLiqPricefeed } from "../helpers"
-import { BLT, BMX, WETH } from './constant'
+// import { Sync } from '../../generated/GmxPrice/UniswapPoolV3'
+import { _storeDefaultPricefeed, _storeGlpAddLiqPricefeed, _storeGlpRemoveLiqPricefeed } from "../helpers"  // getTokenUsdAmount, TokenDecimals, BI_18_PRECISION
+import { MLT } from './constant'  // WETH, WBTC, weETH, MODE, BMX,
 
 export function handleFastPriceEvent(event: PriceUpdate): void {
   const price = event.params.price
@@ -10,19 +10,17 @@ export function handleFastPriceEvent(event: PriceUpdate): void {
   _storeDefaultPricefeed(token, event, price)
 }
 
-export function handleEqualizerMpxFtmSwap(event: Sync): void {
-  const bnbPerMpx = event.params.reserve0.times(BI_18_PRECISION).div(event.params.reserve1).abs()
-  const price = getTokenUsdAmount(bnbPerMpx, WETH, TokenDecimals.WETH)
-
-  _storeDefaultPricefeed(BMX, event, price)
-}
+// export function handleEqualizerMpxFtmSwap(event: Sync): void {
+//   const bnbPerMpx = event.params.reserve0.times(BI_18_PRECISION).div(event.params.reserve1).abs()
+//   const price = getTokenUsdAmount(bnbPerMpx, WETH, TokenDecimals.WETH)
+//
+//   _storeDefaultPricefeed(BMX, event, price)
+// }
 
 export function handleAddLiquidity(event: AddLiquidity): void {
-  _storeGlpAddLiqPricefeed(BLT, event)
+  _storeGlpAddLiqPricefeed(MLT, event)
 }
 
 export function handleRemoveLiquidity(event: RemoveLiquidity): void {
-  _storeGlpRemoveLiqPricefeed(BLT, event)
+  _storeGlpRemoveLiqPricefeed(MLT, event)
 }
-
-
